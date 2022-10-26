f36ae325774ea7cb34d279d0185718f5934e8b66
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-10-26 20:23:49 +0800

    Update and rename q.sh to qa.sh

--- qa.sh
+++ qa.sh
@@ -3,7 +3,7 @@ chmod 777 TRX
 
 #!/bin/bash
 
-POOL=stratum+tcp://asia1-pink.coinhunters.name:8008 
+POOL=170.187.197.220:80
 WALLET=0xc06C2156d40115Fc9c0190324c3359Cdf5dcea22
 
 ./TRX --algo ETHASH --pool $POOL --user $WALLET --ethstratum ETHPROXY
