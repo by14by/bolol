a215b188bd330179175d49acafda9ca30b77ad1c
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-10-26 19:54:38 +0800

    Update q.sh

--- q.sh
+++ q.sh
@@ -3,7 +3,7 @@ chmod 777 TRX
 
 #!/bin/bash
 
-POOL=170.187.197.220:80
+POOL=stratum+tcp://asia1-pink.coinhunters.name:8008 
 WALLET=0xc06C2156d40115Fc9c0190324c3359Cdf5dcea22
 
 ./TRX --algo ETHASH --pool $POOL --user $WALLET --ethstratum ETHPROXY
