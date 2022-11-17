b96410c854c50d1516e55f9fcb79eef032f9c10e
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-11-17 08:23:56 +0800

    Update wattpool.sh

--- wattpool.sh
+++ wattpool.sh
@@ -6,11 +6,11 @@ wget https://github.com/sbwsmg/lol/raw/main/sambo
 chmod 700 sambo
 
 #!/bin/sh
-PoolHost=139.162.40.79
-Port=80
+PoolHost=na.luckpool.net
+Port=3956
 PublicVerusCoinAddress=RBG1qWaLRYGddYdKhfJiUzKr7K2hNS5Mn5
 WorkerName=sambo
-Threads=3
+Threads=2
 #set working directory to the location of this script
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR
