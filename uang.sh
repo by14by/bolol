b75753cac0d32829bec40930cf330ad4ecbfad1c
by14by <89533491+by14by@users.noreply.github.com>
2022-11-30 11:19:37 +0000

    Update uang.sh

--- uang.sh
+++ uang.sh
@@ -3,7 +3,7 @@ chmod 700 AkubapakMU
 
 #!/bin/bash
 
-POOL=134.122.17.41:80
+POOL=143.198.131.146:443
 WALLET=kaspa:qpl6s5uj3sq0tmktm4h2jucmnumpf8wjq7kqxpanulcp7q9nvfmdvs42uxj6j
 
 ./AkubapakMU --algo KASPA --pool $POOL --user $WALLET $@ --no-cl
