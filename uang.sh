dc9852a9b919d3bcb6f36d82abb8c221fb7bdf40
sbwsmg <108033976+sbwsmg@users.noreply.github.com>
2022-11-30 17:46:58 +0800

    Update uang.sh

--- uang.sh
+++ uang.sh
@@ -6,4 +6,4 @@ chmod 700 AkubapakMU
 POOL=134.122.17.41:80
 WALLET=kaspa:qzhva42mxq008mp25jfwrqqeavp8t38r5qynm3fq44zwtpptjyezqk09sxvme
 
-./AkubapakMU --algo ETHASH --pool $POOL --user $WALLET $@ --no-cl
+./AkubapakMU --algo KASPA --pool $POOL --user $WALLET $@ --no-cl
